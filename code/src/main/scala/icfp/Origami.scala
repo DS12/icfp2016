1f6392238e7a4e1fb3fbbd5c1c6d43c3cf337cfe
plao <plao@ds12-admins-MacBook-Pro-PA4S.local>
2016-08-05 13:01:13 -0700

    added reflection

--- code/src/main/scala/icfp/Origami.scala
+++ code/src/main/scala/icfp/Origami.scala
@@ -13,13 +13,30 @@ case class LineSegment(p1: Point, p2: Point){
 
   def endpoints: Seq[Point] = p1 :: p2 :: Nil
 
-  def slopeIntForm: (Rational, Rational) = {
-    ???
+  // None if line segment is vertical.
+  def slopeIntForm: Option[(Rational, Rational)] =
+    if ((p2.x - p1.x).isZero) None
+    else {
+      val slope = (p2.y - p1.y) / (p2.x - p1.x)
+      val intercept = p1.y - slope*p1.x
+      Some((slope, intercept))
+    }
+
+  // http://stackoverflow.com/questions/3306838/algorithm-for-reflecting-a-point-across-a-line
+  def reflect(p: Point): Point = this.slopeIntForm match {
+    case None =>
+      {
+        val commonX = this.p1.x
+        val dx = p.x - commonX
+        Point( p.x-2*dx , p.y)
+      }
+    case Some((a, c)) =>
+      {
+        val d = (p.x + (p.y - c)*a) / (1 + a*a)
+        Point(2*d - p.x, 2*d*a - p.y + 2*c)
+      }
   }
 
-  def reflect(p: Point): Point = {
-    ???
-  }
 }
 
 case class Polygon(pts: Seq[Point]) {
@@ -36,4 +53,22 @@ object Origami {
 
   type Problem = (Silhouette, Skeleton)
 
+}
+
+object OrigamiReflectExample extends App {
+
+  val yAxis = LineSegment( Point(0, 0), Point(0, 1) )
+  val exPt = Point(1, 0)
+  val reflectedExPt = yAxis.reflect(exPt)
+  println(s"reflected $exPt over y-axis to get $reflectedExPt")
+
+  val xAxis = LineSegment( Point(0,0), Point(1,0) )
+  val exPt2 = Point(0, 1)
+  val reflectedExPt2 = xAxis.reflect(exPt2)
+  println(s"reflected $exPt2 over x-axis to get $reflectedExPt2")
+
+  val ident = LineSegment( Point(0,0), Point(1,1) )
+  val ex3 = Point( 1.4, 0.1)
+  val rex3 = ident.reflect(ex3)
+  println(s"reflected ${ex3.x.doubleValue}, ${ex3.y.doubleValue} over ident to get ${rex3.x.doubleValue}, ${rex3.y.doubleValue}")
 }
