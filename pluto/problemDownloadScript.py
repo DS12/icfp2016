7bf1e4842614b2fc448cae6b179d82fe64746ac0
Kamal Gurala <kamal.eagles2104@gmail.com>
2016-08-05 16:06:17 -0700

    First 5% and Last 5% done mates. cheers

--- pluto/problemDownloadScript.py
+++ pluto/problemDownloadScript.py
@@ -6,26 +6,21 @@ import operator
 import requests
 
 header = {'Expect': '', 'X-API-Key': '69-69f9cd695ba4358b4b56209a54c821db',}
-a = requests.get('http://2016sv.icfpcontest.org/api/snapshot/list', headers=header)
-b = json.loads(a.text)
-newlist = sorted(b["snapshots"], key=lambda k: k['snapshot_time'])
+snapshot = requests.get('http://2016sv.icfpcontest.org/api/snapshot/list', headers=header)
+snapshotList = json.loads(snapshot.text)
+newlist = sorted(snapshotList["snapshots"], key=lambda k: k['snapshot_time'])
 newlist.reverse()
-ha = newlist[0]["snapshot_hash"]
+latestSnap = newlist[0]["snapshot_hash"]
 time.sleep(2)
-print ha
-yolo =  requests.get('http://2016sv.icfpcontest.org/api/blob/'+str(ha), headers=header)
-print yolo.text
-probs = json.loads(yolo.text)
-print probs
-hashes = list()
-for problem in probs["problems"]:
-    hashes.append(problem["problem_spec_hash"])
+latestReq =  requests.get('http://2016sv.icfpcontest.org/api/blob/'+str(latestSnap), headers=header)
+problems = json.loads(latestReq.text)["problems"]
+problemId = list()
+
+for problem in problems:
+    problemId.append((problem["problem_spec_hash"],problem["problem_id"]))
 
 f = open('hashes.txt','w')
-for hs in hashes:
-    f.write("%s\n" % hs)
-
-for i, val in enumerate(hashes):
-    time.sleep(1)
-    print("curl --compressed -L -H Expect: -H 'X-API-Key: 69-69f9cd695ba4358b4b56209a54c821db' 'http://2016sv.icfpcontest.org/api/blob/" + val[:-2] +"' > problems/"+val[:-2]+".txt")
-    os.system("curl --compressed -L -H Expect: -H 'X-API-Key: 69-69f9cd695ba4358b4b56209a54c821db' 'http://2016sv.icfpcontest.org/api/blob/" + val[:-2] +"' > problems/" +val[:-2] + ".txt")
+for i in problemId:
+    f.write("%s\n" % i[1])
+    time.sleep(2)
+    os.system("curl --compressed -L -H Expect: -H 'X-API-Key: 69-69f9cd695ba4358b4b56209a54c821db' 'http://2016sv.icfpcontest.org/api/blob/" + str(i[0]) +"' > problems/" + str(i[1]) + ".txt")
