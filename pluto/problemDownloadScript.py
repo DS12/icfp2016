64885878aea1fa089d91d46b8d9112bc821198dc
Kamal Gurala <kamal.eagles2104@gmail.com>
2016-08-05 15:03:36 -0700

    Updated shit here and there

--- pluto/problemDownloadScript.py
+++ pluto/problemDownloadScript.py
@@ -2,11 +2,30 @@ import json
 from pprint import pprint
 import time
 import os
+import operator
+import requests
 
-with open("hashes.txt") as f:
-    hashes = f.readlines()
+header = {'Expect': '', 'X-API-Key': '69-69f9cd695ba4358b4b56209a54c821db',}
+a = requests.get('http://2016sv.icfpcontest.org/api/snapshot/list', headers=header)
+b = json.loads(a.text)
+newlist = sorted(b["snapshots"], key=lambda k: k['snapshot_time'])
+newlist.reverse()
+ha = newlist[0]["snapshot_hash"]
+time.sleep(2)
+print ha
+yolo =  requests.get('http://2016sv.icfpcontest.org/api/blob/'+str(ha), headers=header)
+print yolo.text
+probs = json.loads(yolo.text)
+print probs
+hashes = list()
+for problem in probs["problems"]:
+    hashes.append(problem["problem_spec_hash"])
+
+f = open('hashes.txt','w')
+for hs in hashes:
+    f.write("%s\n" % hs)
 
 for i, val in enumerate(hashes):
     time.sleep(1)
-    print("curl --compressed -L -H Expect: -H 'X-API-Key: 69-69f9cd695ba4358b4b56209a54c821db' 'http://2016sv.icfpcontest.org/api/blob/" + val[:-2] +"' > p" + str(i) + ".txt")
-    os.system("curl --compressed -L -H Expect: -H 'X-API-Key: 69-69f9cd695ba4358b4b56209a54c821db' 'http://2016sv.icfpcontest.org/api/blob/" + val[:-2] +"' > p" + str(i) + ".txt")
+    print("curl --compressed -L -H Expect: -H 'X-API-Key: 69-69f9cd695ba4358b4b56209a54c821db' 'http://2016sv.icfpcontest.org/api/blob/" + val[:-2] +"' > problems/"+val[:-2]+".txt")
+    os.system("curl --compressed -L -H Expect: -H 'X-API-Key: 69-69f9cd695ba4358b4b56209a54c821db' 'http://2016sv.icfpcontest.org/api/blob/" + val[:-2] +"' > problems/" +val[:-2] + ".txt")
