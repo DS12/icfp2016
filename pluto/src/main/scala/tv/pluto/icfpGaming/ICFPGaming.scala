a72cbda4cbcf383d045a9439553fc952413a44ac
Kamal Gurala <kamal.eagles2104@gmail.com>
2016-08-05 16:56:22 -0700

    Added Mirroring about a LIne segment

--- pluto/src/main/scala/tv/pluto/icfpGaming/ICFPGaming.scala
+++ pluto/src/main/scala/tv/pluto/icfpGaming/ICFPGaming.scala
@@ -1,7 +1,9 @@
 package tv.pluto.icfpGaming
 
+import spire.math.Rational
 import tv.pluto.icfp._
 import tv.pluto.icfp.Parser._
+
 import sys.process._
 import scala.io.Source
 
@@ -88,6 +90,17 @@ object ICFPGaming {
     val y = centroid.y
 
     List(Point(x-0.5, y-0.5), Point(x+0.5, y-0.5), Point(x+0.5, y+0.5), Point(x-0.5, y+0.5))
+  }
+
+  def mirrorPoint(l1: Point, l2:Point, p1: Point): Point ={
+    val slope: Rational = (l2.y - l1.y)/(l2.x - l1.x)
+    val intercept: Rational = l1.y - slope*l1.x
+    val d: Rational = (p1.x + (p1.y - intercept)*slope) / (1+slope*slope)
+
+    val x: Rational = d*2 - p1.x
+    val y: Rational = d*2*slope - p1.y + intercept*2
+
+    Point(x,y)
   }
 
 }
