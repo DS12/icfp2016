61cf11b04301f190544c0a13cdb88e536c5c9fd5
Kamal Gurala <kamal.eagles2104@gmail.com>
2016-08-06 16:16:09 -0700

    Under Pressure

--- pluto/problemSubmitScript.py
+++ pluto/problemSubmitScript.py
@@ -5,8 +5,13 @@ import os
 import operator
 import requests
 
-f = open('hashes.txt','r')
-for prob in f.readlines():
+def remove(x):
+    return x[:-4]
+
+a = map(remove,os.open("./solutionsRect"))
+
+
+for prob in a:
     i = prob[:-1]
     time.sleep(2)
-    os.system("curl --compressed -L -H Expect: -H 'X-API-Key: 69-69f9cd695ba4358b4b56209a54c821db' -F 'problem_id="+str(i)+"' -F 'solution_spec=@solutions/"+str(i)+".txt' 'http://2016sv.icfpcontest.org/api/solution/submit' > results/"+str(i)+".txt")
+    print("curl --compressed -L -H Expect: -H 'X-API-Key: 69-69f9cd695ba4358b4b56209a54c821db' -F 'problem_id="+str(i)+"' -F 'solution_spec=@solutions/"+str(i)+".txt' 'http://2016sv.icfpcontest.org/api/solution/submit' > results/"+str(i)+".txt")
