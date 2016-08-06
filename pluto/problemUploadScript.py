1cb5b5762af29affa9fc91409ff623718f3c58b9
Kamal Gurala <kamal.eagles2104@gmail.com>
2016-08-05 19:45:03 -0700

    The script

---
+++ pluto/problemUploadScript.py
@@ -0,0 +1,13 @@
+import json
+from pprint import pprint
+import time
+import os
+import operator
+import requests
+
+baseTime = 1470441600
+diffTime = 3600
+uploadTime = baseTime + diffTime*4
+for i in range(14):
+    os.system("curl --compressed -L -H Expect: -H 'X-API-Key: 69-69f9cd695ba4358b4b56209a54c821db' -F 'solution_spec=@uploads/"+ str(i) +".txt' -F 'publish_time="+str(uploadTime)+"' 'http://2016sv.icfpcontest.org/api/problem/submit'")
+    uploadTime = uploadTime + 3600
