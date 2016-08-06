107195bb2cc9f56d21a9dbc4156bd423f99a751b
Kamal Gurala <kamal.eagles2104@gmail.com>
2016-08-06 15:27:18 -0700

    Updatet Problemsas of 3:30

--- pluto/problemDownloadScript.py
+++ pluto/problemDownloadScript.py
@@ -13,14 +13,28 @@ newlist.reverse()
 latestSnap = newlist[0]["snapshot_hash"]
 time.sleep(2)
 latestReq =  requests.get('http://2016sv.icfpcontest.org/api/blob/'+str(latestSnap), headers=header)
+
+
 problems = json.loads(latestReq.text)["problems"]
+problemIdHash = list()
 problemId = list()
 
 for problem in problems:
-    problemId.append((problem["problem_spec_hash"],problem["problem_id"]))
+    problemId.append(problem["problem_id"])
+    problemIdHash.append((problem["problem_spec_hash"],problem["problem_id"]))
 
-f = open('hashes.txt','w')
-for i in problemId:
-    f.write("%s\n" % i[1])
-    time.sleep(2)
-    os.system("curl --compressed -L -H Expect: -H 'X-API-Key: 69-69f9cd695ba4358b4b56209a54c821db' 'http://2016sv.icfpcontest.org/api/blob/" + str(i[0]) +"' > problems/" + str(i[1]) + ".txt")
+
+
+def remove(x):
+    return x[:-4]
+downloadedProblems = map(remove,os.listdir("./problems"))
+
+newProblemsId = [x for x in problemId if str(x) not in downloadedProblems]
+print newProblemsId
+
+for i in problemIdHash:
+    if(i[1] in newProblemsId):
+        time.sleep(1.2)
+        print(str(i[0]) +"' > problems/" + str(i[1]) + ".txt")
+        os.system("curl --compressed -L -H Expect: -H 'X-API-Key: 69-69f9cd695ba4358b4b56209a54c821db' 'http://2016sv.icfpcontest.org/api/blob/" + str(i[0]) +"' > problems/" + str(i[1]) + ".txt")
+
