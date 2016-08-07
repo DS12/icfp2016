77249ac0a9a8f7932708cf807488c17ba311a9d6
Vojtech Letal <letal.vojtech@gmail.com>
2016-08-07 09:38:41 -0700

    PLUTO add latest problems and results

--- pluto/problemSubmitScript.py
+++ pluto/problemSubmitScript.py
@@ -1,16 +1,24 @@
-import json
-from pprint import pprint
-import time
 import os
-import operator
-import requests
+import time
+
 
 def remove(x):
     return x[:-4]
 
-a = map(remove,os.listdir("./solutionsRect"))
 
+solutions = set(os.listdir("./solutionsRect"))
+already_submitted = set(os.listdir("./results"))
 
-for i in a:
+print "Already submitted problems:", len(already_submitted)
+print "Solved problems:", len(solutions)
+print "New solutions to submit:", len(solutions - already_submitted)
+
+ids = map(remove, solutions - already_submitted)
+
+for i in ids:
     time.sleep(1.25)
-    os.system("curl --compressed -L -H Expect: -H 'X-API-Key: 69-69f9cd695ba4358b4b56209a54c821db' -F 'problem_id="+str(i)+"' -F 'solution_spec=@solutionsRect/"+str(i)+".txt' 'http://2016sv.icfpcontest.org/api/solution/submit' > results/"+str(i)+".txt")
+    os.system(
+        "curl --compressed -L -H Expect: -H 'X-API-Key: 69-69f9cd695ba4358b4b56209a54c821db' -F 'problem_id=" + str(
+            i) + "' -F 'solution_spec=@solutionsRect/" + str(
+            i) + ".txt' 'http://2016sv.icfpcontest.org/api/solution/submit' > results/" + str(i) + ".txt"
+    )
